2a60b46be131d9feeae74b07a5168fd06eaa8c0c
Dmytro <dmytrostrembetskyi@gmail.com>
2022-01-05 13:51:38 +0200

    complete module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -32,36 +32,22 @@ namespace StarChart.Controllers
         [HttpGet("{name}")]
         public IActionResult GetByName(string name)
         {
-            var item = _context.CelestialObjects.FirstOrDefault(i => i.Name == name);
-            if (item == null) return NotFound();
-            var satellites = _context.CelestialObjects.Where(i => i.OrbitedObjectId == item.Id);
-            item.Satellites = satellites.ToList();
-            return Ok(item);
+            var items = _context.CelestialObjects.Where(i => i.Name == name).ToList();
+            if (!items.Any()) return NotFound();
+
+            items.ForEach(
+                i => i.Satellites = _context.CelestialObjects.Where(i2 => i2.OrbitedObjectId == i.Id).ToList());
+            return Ok(items);
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            // var items = _context.CelestialObjects.ToList();
-            // var satellites = _context.CelestialObjects.Where(i => i.OrbitedObjectId == null);
-            // items.ForEach(i =>
-            // {
-            //     if(i.)
-            // });
-            SeedDatabase();
-            
-            // var query = _context.CelestialObjects
-            //     .Join(
-            //         _context.CelestialObjects, 
-            //         c1 => c1.OrbitedObjectId,
-            //         c2 => c2.Id,
-            //     (c1, c2) => c2
-            //         );
-            // var a2 = query.ToList();
+            var items = _context.CelestialObjects.ToList();
+            items.ForEach(
+                i => i.Satellites = _context.CelestialObjects.Where(i2 => i2.OrbitedObjectId == i.Id).ToList());
 
-            var a3 = _context.CelestialObjects.Include(i => i.Satellites).ToList();
-            
-            return Ok(a3);
+            return Ok(items);
         }
 
         private void SeedDatabase()
@@ -72,7 +58,7 @@ namespace StarChart.Controllers
             var fakeItem2 =
                 new CelestialObject {Id = 2, Name = "two", OrbitalPeriod = TimeSpan.Zero, OrbitedObjectId = 1};
             _context.CelestialObjects.Add(fakeItem2);
-            
+
             var fakeItem3 = new CelestialObject {Id = 3, Name = "hello", OrbitalPeriod = TimeSpan.Zero,};
             _context.CelestialObjects.Add(fakeItem3);
 
